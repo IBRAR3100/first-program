5161d80446ffc0756b992b9fbde9d734f0a3e99c
IBRAR3100 <ibrarawan8672@gmail.com>
2025-04-23 17:12:47 +0300

    Add files via upload

---
+++ pythonfirst.py
@@ -0,0 +1,41 @@
+#firstname="ali"
+#*print(firstname)
+#fruit="peach"
+#print(fruit)
+#sports="cricket,football,hockey"
+#print(sports)er)
+weather="summer,winter"
+print(weather)
+books="math,accounting,commerce,"
+print(books)
+country="PAKISTAN,QATAR"
+print(country)
+age=20
+year=2024
+numberofstudent=55
+print(age)
+print(year)
+print(numberofstudent)
+weight=50  #kg
+milkquantity=10 #in liter
+temperature=35 #in celsius
+print(weight)
+print(milkquantity)
+print(temperature)
+name="ali"
+studentname="aliahmed"
+place="sialkot"
+phonenumber="033312345678"
+print(name)
+print(studentname)
+print(place)
+print(phonenumber)
+is_weekend=True
+on_vacation=False
+print(is_weekend)
+print(on_vacation)
+
+
+
+
+
